f67456fc3bf883c11e92185e6925933230eea08c
CIMAN01 <34729011+CIMAN01@users.noreply.github.com>
2019-12-01 17:57:32 -0800

    Update U2.java

--- U2.java
+++ U2.java
@@ -1,87 +1,81 @@
-/**  Udacity Java Course - Space Challenge Project: Mission To Mars
- **  by CIMAN01 (December 2017)
+/**
+ ** Udacity Java Course - Space Challenge Project: Mission To Mars
+ ** by CIMAN01 (December 2017 / modified November 2019)
  **/
 
 
 /**
-
-The U2 Rocket heavier than the U-1 but much safer and can carry a lot more cargo; to a total of 29 tonnes.
-However, it costs $120 Million to build and weighs 20 tonnes. It has a greater chance of crashing while
-landing than while launching, but just like the U-1 both chances depend on the amount of cargo carried.
-
-Rocket Specifications:
------------------------------------------------------------------
-Rocket cost = $120 Million
-Rocket weight = 20 Tonnes
-Max weight (with cargo) = 29 Tonnes
-Chance of launch explosion = 4% * (cargo carried / cargo limit)
-Chance of landing crash = 8% * (cargo carried / cargo limit)
------------------------------------------------------------------
-
-Create classes U1 and U2 that extend the Rocket class and override the land and launch methods
-to calculate the corresponding chance of exploding and return either true or false
-based on a random number using the probability equation for each.
-
-**/
-
-import java.lang.*;
+ The U2 Rocket heavier than the U-1 but much safer and can carry a lot more cargo; to a total of 29 tonnes.
+ However, it costs $120 Million to build and weighs 20 tonnes. It has a greater chance of crashing while
+ landing than while launching, but just like the U-1 both chances depend on the amount of cargo carried.
+ Rocket Specifications:
+ -----------------------------------------------------------------
+ Rocket cost = $120 Million
+ Rocket weight = 20 Tonnes
+ Max weight (with cargo) = 29 Tonnes
+ Chance of launch explosion = 4% * (cargo carried / cargo limit)
+ Chance of landing crash = 8% * (cargo carried / cargo limit)
+ -----------------------------------------------------------------
+ Create classes U1 and U2 that extend the Rocket class and override the land and launch methods
+ to calculate the corresponding chance of exploding and return either true or false
+ based on a random number using the probability equation for each.
+ **/
 
 public class U2 extends Rocket {
+    // U2 instance data
+    private double chanceOfExplosion; // Chance of launch explosion
+    private double chanceOfCrash; // Chance of landing crash
+    private float percentLift; // percentage
+    private float percentCrash; // percentage
 
-    // Constructor
+    // U2 constructor
     public U2() {
-
-        rocketCost = 120; // rocket Cost = $120 Million
-        rocketWeight = 20000; // rocket Weight = 20000 kilograms
-        maxWeight = 29000; // Max weight (with cargo) = 29000 kilograms
-        currentRocketWeight = rocketWeight;
-        cargoLimit = maxWeight;
-        cargoCarried = currentRocketWeight;
-
+        setRocketCost(120); // rocket Cost = $100 Million
+        setRocketWeight(20000); // rocket Weight = 10000 kilograms
+        setMaxWeight(29000); // Max weight (with cargo) = 18000 kilograms
+        setCurrentRocketWeight(20000); // same as rocket weight
+        chanceOfExplosion = 0;
+        chanceOfCrash = 0;
+        percentLift = 0;
+        percentCrash = 0;
     }
-
     // override the launch method to calculate the corresponding chance of exploding
+    @Override
     public boolean launch() {
-
-        // Random Number between 0 and 0.999...
+        // random number between 0 and 0.999...
         double chance = Math.random();
-
-        // Chance of launch explosion = 4% * (cargo carried / cargo limit)
-        percent = 4/100f;
-        chanceOfExplosion = percent *  ( (double)cargoCarried / (double)cargoLimit);
-
-        if (chanceOfExplosion >= chance) {
+        // 4% or 0.04
+        this.percentLift = 4/100f;
+        // chance of launch explosion = 4% * (cargo carried / cargo limit)
+        this.chanceOfExplosion = this.percentLift * ( (double) getCurrentRocketWeight() / getMaxWeight() );
+        // if chance of failure is high enough the rocket will explode after launch
+        if (this.chanceOfExplosion >= chance) {
             System.out.println("U2 Exploded!");
             return false;
+        // otherwise the rocket will launch successfully
         } else {
-            System.out.println("U2 Launched Successfully");
+            System.out.println("nU2 Launched Successfully");
+            return true;
         }
-
-        return true;
-
     }
 
     // override the land method to calculate the corresponding chance of crash
+    @Override
     public boolean land() {
-
-        // Random Number between 0 and 0.999...
+        // random Number between 0 and 0.999...
         double chance = Math.random();
-
-        // Chance of landing crash = 8% * (cargo carried / cargo limit)
-        percent = 8/100f;
-        chanceOfCrash = percent * ((double) cargoCarried / (double) cargoLimit);
-
-        if (chanceOfCrash >= chance) {
+        // 8% or 0.08
+        this.percentCrash = 8/100f;
+        // chance of landing crash = 8% * (cargo carried / cargo limit)
+        this.chanceOfCrash = this.percentCrash * ( (double) getCurrentRocketWeight() / getMaxWeight() );
+        // if chance of failure is high enough the rocket will crash upon landing
+        if (this.chanceOfCrash >= chance) {
             System.out.println("U2 Crashed!");
             return false;
-
+        // otherwise the rocket will make it to mars successfully
         } else {
             System.out.println("U2 Landed Successfully");
+            return true;
         }
-
-        return true;
     }
-
 }
-
-
