58b14f6309940488e70060dc616952e9f0bbb7ff
CIMAN01 <34729011+CIMAN01@users.noreply.github.com>
2019-12-01 17:52:57 -0800

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,14 +1,14 @@
-/**  Udacity Java Course - Space Challenge Project: Mission To Mars
- **  by CIMAN01 (December 2017)
+/** Udacity Java Course - Space Challenge Project: Mission To Mars
+ ** by CIMAN01 (December 2017 / modified November 2019)
  **/
 
 // Create an Item class that includes a String name and an int weight that will represent an item
 // to be carried by the rockets
 
 public class Item {
-
-    public String name;        // name of the item to be carried
-    public int weight;         // weight of the item to be carried
+    // instance variables
+    private String name; // name of the item to be carried
+    private int weight; // weight of the item to be carried
 
     // Constructor
     public Item(String name, int weight) {
@@ -16,4 +16,23 @@ public class Item {
         this.weight = weight;
     }
 
+    // name instance setter method
+    void setName(String name) {
+        this.name = name;
+    }
+
+    // weight instance setter methods
+    void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    // name instance getter method
+    public String getName() {
+        return name;
+    }
+
+    // weight instance getter method
+    public int getWeight() {
+        return weight;
+    }
 }
