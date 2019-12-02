0d98bb269b0f1a8f8dc68cd302fd094d5001c862
CIMAN01 <34729011+CIMAN01@users.noreply.github.com>
2019-12-01 17:56:25 -0800

    Update SpaceShip.java

--- SpaceShip.java
+++ SpaceShip.java
@@ -1,26 +1,19 @@
-/**  Udacity Java Course - Space Challenge Project: Mission To Mars
- **  by CIMAN01 (December 2017)
+/** Udacity Java Course - Space Challenge Project: Mission To Mars
+ ** by CIMAN01 (December 2017 / modified November 2019)
  **/
 
 
 /**
-
  Create a SpaceShip Interface that includes the definitions of these methods:
-
-1. launch: a method that returns either true or false indicating if the launch was successful
-           or if the rocket has crashed.
-
-2. land: a method that also returns either true or false based on the success of the landing.
-
-3. canCarry: a method that takes an Item as an argument and returns true if the rocket can carry
-             such item or false if it will exceed the weight limit.
-
-4. carry: a method that also takes an Item object and updates the current weight of the rocket.
-
-**/
+ 1. launch: a method that returns either true or false indicating if the launch was successful
+ or if the rocket has crashed.
+ 2. land: a method that also returns either true or false based on the success of the landing.
+ 3. canCarry: a method that takes an Item as an argument and returns true if the rocket can carry
+ such item or false if it will exceed the weight limit.
+ 4. carry: a method that also takes an Item object and updates the current weight of the rocket.
+ **/
 
 public interface SpaceShip {
-
     // launch method will return true if rocket takes off or false if it crashes.
     boolean launch();
 
@@ -31,7 +24,6 @@ public interface SpaceShip {
     // such item or false if it exceeds the maximum weight limit.
     boolean canCarry(Item item);
 
-    // carry method takes an Item object and updates the current weight of the rocket.
-    int carry(Item item);
-
+    // carry method takes an item object and updates the current weight of the rocket.
+    void carry(Item item);
 }
